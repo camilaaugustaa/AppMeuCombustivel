f01a250464ca380d8a850633f465c6f597b94a5d
camilaaugustaa <camilaaugustasnt@outlook.com>
2024-11-25 22:43:24 -0300

    Adicionar arquivos de projeto.

---
+++ AppMeuCombustivel/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppMeuCombustivel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppMeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace AppMeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+     
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+                //O preço do etanol seja menor do que 70% do preço da gasolina.
+
+                string msg = "";
+
+                if (etanol <= (gasolina * 0.7))
+                {
+                    msg = "O Etanol está compensando.";
+                } else
+                {
+                    msg = "A Gasolina está compensando.";
+                }
+
+                DisplayAlert("Calculado", msg, "Ok.");
+
+            } catch (Exception ex)
+            {
+                DisplayAlert("Ops!", ex.Message, "Fechar");
+            }
+        }
+    }
+
+}
